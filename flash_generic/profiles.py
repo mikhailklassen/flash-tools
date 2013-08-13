1a47e35067a9bc7347f82f889cbe8722fe809b7e
Mikhail Klassen <mikhail.klassen@gmail.com>
2013-08-13 15:49:03 -0400

    Turns out that the way I was using yt's TimeSeries commands and iterating through the plotsfiles was causing me to run out of memory. I've changed the routine now to reading the files in turn and overwriting the previous arrays. I also generate plots along the way now so that I can get some output at least, even if it crashes or gets interrupted.

--- flash_generic/profiles.py
+++ flash_generic/profiles.py
@@ -9,37 +9,49 @@ def density_profile_1D_evolution(files,outdir):
     pc   = 3.08568025e18
     AU   = 1.49598e13
     Rsun = 6.955e10
+    scale = select_scale(6.17e18)
     
-    storage = {}
-
-    ts = ytm.TimeSeriesData.from_filenames(files)
-
-    for sto,pf in ts.piter(storage=storage):
-        a = ytm.PlotCollection(pf,center=[0.5,0.5,0.5]/pf["unitary"]).add_profile_sphere(0.5, "unitary",["Radius","Density"], weight="CellMassMsun")
-        sto.result = a.data
-    
-    numfiles = len(files)
-    greys = linspace(0.8,0,numfiles)
-    colors = [[greys[i],greys[i],greys[i]] for i in range(numfiles)]
+    #ts = ytm.TimeSeriesData.from_filenames(files)
 
     fig = plt.figure()
     ax = plt.subplot(1,1,1)
-    for i in storage:
-        scale = select_scale(6.17e18)
-        radii = storage[i]['Radius']*pf[scale]
-        densities = storage[i]['Density']
-        if i == len(storage) - 1:
-            ax.semilogy(radii,densities,color=colors[i],label="Radial density profile")
-        else:
-            ax.semilogy(radii,densities,color=colors[i])
-
-    if scale == 'Rsun': scale = r'$R_{\odot}$'
-    ax.set_xlim((0,0.5*pf[scale]/pf["unitary"]))
-    ax.set_xlabel(r'Radius [{0}]'.format(scale))
+    ax.set_xlabel(r'Radius [{0}]'.format(r'$R_{\odot}$' if scale == 'Rsun' else scale))
     ax.set_ylabel(r'Density [g/cm$^3$]')
     ax.grid(True)
-    ax.legend()
    
+    numfiles = len(files)
+    greys = linspace(0.8,0,numfiles)
+    colors = [[greys[i],greys[i],greys[i]] for i in range(numfiles)]
+    
+    for i, file in enumerate(files):
+        print 'Processing file {0} of {1}: {2}'.format(i+1,len(files),file)
+        pf = ytm.load(file)
+        a = ytm.PlotCollection(pf,center=[0.5,0.5,0.5]/pf["unitary"]).add_profile_sphere(0.5, "unitary",["Radius","Density"], weight="CellMassMsun")
+        radii, densities = a.data['Radius']*pf[scale], a.data['Density']
+        if i == numfiles - 1:
+            ax.semilogy(radii,densities,color=colors[i],label="Radial average density profile")
+            ax.legend()
+        else:
+            ax.semilogy(radii,densities,color=colors[i])
+        if i == 1:
+            ax.set_xlim((0,0.5*pf[scale]/pf["unitary"]))
+        plt.savefig(outdir+'/'+'temp_radial_density_profile.png')
+
+#    for sto,pf in ts.piter(storage=storage):
+#        print 'Processing file {0} of {1}: {2}'.format(i+1,len(files),pf.basename)
+#        a = ytm.PlotCollection(pf,center=[0.5,0.5,0.5]/pf["unitary"]).add_profile_sphere(0.5, "unitary",["Radius","Density"], weight="CellMassMsun")
+#        sto.result = a.data
+#        i+=1
+#
+#    for i in storage:
+#        scale = select_scale(6.17e18)
+#        radii = storage[i]['Radius']*pf[scale]
+#        densities = storage[i]['Density']
+#        if i == len(storage) - 1:
+#            ax.semilogy(radii,densities,color=colors[i],label="Radial density profile")
+#        else:
+#            ax.semilogy(radii,densities,color=colors[i])
+
     formats = ['png','eps','pdf']
     for type in formats:
         plt.savefig(outdir+'/'+'radial_density_profile.'+type,format=type)
